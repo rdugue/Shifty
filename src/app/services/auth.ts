22e5b7e717b71bae5a8cdcca6235d1a3bc951c65
Ralph Dugue <rdugue1@gmail.com>
2017-05-28 20:09:30 -0400

    minor refactoring of User and Shift classes for testing with AWS

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -45,12 +45,9 @@ export class AuthService implements CanActivate {
     }
 
     authenticate(path, creds): Observable<any> {
-        return this.api.post(`/api/${path}`, creds)
+        return this.api.post(path, creds)
         .do((res: any) => this.setJwt(res.token))
-        .do((res: any) => {
-            console.log('Auth response data:', res.data);
-            this.storeHelper.update('user', res.data);
-        })
+        .do((res: any) => this.storeHelper.update('user', res.data))
         .map((res: any) => res.data);
     }
 
